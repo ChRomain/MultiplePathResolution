684681bfa2e8209e92cba261ebdda83bb51330df
rcharret <charretteur.r@gmail.com>
2018-11-09 14:03:50 +0000

    full update

--- README.md
+++ README.md
@@ -1,29 +1,48 @@
-## multiplPathResol
-Some results could be found in results.log for various environment (prisma 15.3 vs 13.3, mySql vs PostgreSql)
+## multiple Path Resol
+Received an error when try to query parents data for two types but with one missing in the first type.
 
-
-NoSuchElementException: ke not found on multiple path resolution
+Error message received from query: "Whoops. Looks like an internal server error. Search your server logs for request ID: local:api:{ID}"
 
 **Repoducibility/Probability :**
   always fail
 
 ## To reproduce:
 **Clone the repository:**
-git clone https://github.com/mjamelot/MutationIntensive.git
-cd multiplPathResol/
+git clone https://github.com/ChRomain/MultiplePathResolution.git
+cd multiplePathResol/
 npm install
 
 **Start the server**
 npm-run prisma deploy
 
-
 **get prisma token and copy in the index.js**
 npm-run prisma token
-
-
 
 **run test**
 node index.js
 
 **result of test**
 
+**Test1**
+1. Create 1 group with a car and a truck.
+2. Create 1 characteristic with the previous car and previous truck.
+3. Query the previous group and get the same parents data for the car and truck.
+**OK**
+
+**Test2**
+1. Create 1 group with a car and a truck.
+2. Create 1 characteristic with the previous car and previous truck.
+3. Query the previous group and get different parents data for the car and truck.
+**KO**
+Observe trace  received from Query :
+  "errors": [
+    {
+      "message": "Whoops. Looks like an internal server error. Search your server logs for request ID: local:api:cjoa36t7ia8zr0986iat0x0vx",
+      "path": [
+        "KO",
+        "truck",
+        "referencedByCharacteristicDatas",
+        0,
+        "parentCharacteristic",
+        "name"
+      ],
--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const { exec } = require('child_process');
 const { GraphQLClient } = require('graphql-request');
 
 
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InNlcnZpY2UiOiJsaW5rZWRMaXN0QGRldiIsInJvbGVzIjpbImFkbWluIl19LCJpYXQiOjE1MzgzODY0OTAsImV4cCI6MTUzODk5MTI5MH0.mslSgOyTLok1BPfkFh6zXSvnt830A7UZlPdEJ8tLcwY"
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InNlcnZpY2UiOiJsaW5rZWRMaXN0QGRldiIsInJvbGVzIjpbImFkbWluIl19LCJpYXQiOjE1NDE3NTU1MTQsImV4cCI6MTU0MjM2MDMxNH0.G_2-faPKQ1Ax9fG2SH42CcdLLju86xABrPEwhOM6tms"
 
 
-const client = new GraphQLClient('http://localhost:4466/linkedList/dev', {
+const client = new GraphQLClient('http://localhost:4466/multiplPathRes/dev', {
   headers: {
     Authorization: `Bearer ${token}`,
   },
@@ -20,61 +20,112 @@ console.debug = function (args) {
   }
 }
 
-
-let createChain = (usersIds) => `mutation {
-  item1: createItem(data: {
-    name:"UsersIds ${usersIds}"
-    label:"myLabel"  
-  })
-  {
-    id
-  }
-}`;
-
-async function deleteManyItmes() {
-  return await client.request(`mutation{deleteManyItems{count}}`);
+async function createGroup() {
+  return await client.request(`mutation {
+    createGroup (
+      data: {
+        name: "nameGroup"
+        car: {
+          create: {
+            name: "nameCar"
+          }
+        }
+        truck: {
+          create: {
+            name: "nameTruck"
+          }
+        }
+      }) 
+    {
+      id
+      car {
+        id
+      }
+      truck {
+        id
+      }
+    }
+  }`);
 }
 
-async function mutation(userId, sync) {
-  let result;
-  try {
-    result = await client.request(createChain(userId));
-    console.log(JSON.stringify(result));
-  } catch (e) {
-    console.log(`error mutation prisma ${sync?'sync':'async'}` + JSON.stringify(e));
-
-  }
-  return result;
+async function createCharacteristic(carId, truckId) {
+  return await client.request(`mutation {
+    createCharacteristic (
+      data: {
+        name: "nameCharact"
+          datas: {
+          create: {
+            carValue: {connect: {id: "${carId}"}}
+            truckValue: {connect: {id: "${truckId}"}}
+          }
+        }
+      })
+    {
+      id
+    }
+  }`);
 }
 
-async function aSynchronousMutation(userIdList) {
-  try {
-    await Promise.all(userIdList.map(userId => mutation(userId,false)));
-  } catch (e) {
-    console.log("error" + JSON.stringify(e));
-  }
-  const deleteCount = await deleteManyItmes();
-  console.log(`delete all aynchronous objet count${JSON.stringify(deleteCount)}`);
+async function queryOK(groupId) {
+  return await client.request(`{
+    OK: group (where: {id:"${groupId}"}) {
+      id
+      car {
+        referencedByCharacteristicDatas {
+          parentCharacteristic {
+            id
+            name
+          }
+        }
+      }
+      truck {    
+          referencedByCharacteristicDatas {
+          parentCharacteristic {
+              id
+              name
+          }
+          }
+      }
+    }
+  }`);
 }
 
-
-async function synchronousMutation(userIdList) {
-  for (var i = 0; i < userIdList.length; i++) { await mutation(userIdList[i],false) }
-  const deleteCount = await deleteManyItmes();
-  console.log(`delete all synchronous objet count${JSON.stringify(deleteCount)}`);
+async function queryKO(groupId) {
+  return await client.request(`{
+    KO: group (where: {id:"${groupId}"}) {
+      id
+      car {
+        referencedByCharacteristicDatas {
+          parentCharacteristic {
+            id
+            
+          }
+        }
+      }
+      truck {    
+          referencedByCharacteristicDatas {
+          parentCharacteristic {
+              id
+              name
+          }
+          }
+      }
+    }
+  }`);
 }
 
 async function main() {
-  await deleteManyItmes();
-  const loops = 3000;
-  let userIdList = [];
-  for (var i = 0; i < loops; i++) { userIdList.push(i); }
 
-  await synchronousMutation(userIdList);
+  const group = await createGroup();
+  const groupId = group.createGroup.id;
+  const carId = group.createGroup.car.id;
+  const truckId = group.createGroup.truck.id;
 
-  await aSynchronousMutation(userIdList);
+  const characteristic = await createCharacteristic(carId, truckId);
+  const characteristicId = characteristic.createCharacteristic.id;
 
-
+  await queryOK(groupId);
+  await queryKO(groupId);
 }
 
 main();
